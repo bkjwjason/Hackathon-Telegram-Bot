43a1d2d0ac67ec38614dd8059cbb2e40c75c020f
Rayner Toh <raynertoh@r-171-104-25-172.comp.nus.edu.sg>
2022-07-09 16:45:07 +0800

    Modified file structure

--- firebase/main.py
+++ firebase/main.py
@@ -1,7 +1,7 @@
 import firebase_admin
 import json
-import sys 
 from firebase_admin import db
+
 
 cred_obj = firebase_admin.credentials.Certificate(
     "help-a-neighbour-out-firebase-adminsdk-3k1ih-a01592b112.json")
@@ -11,14 +11,10 @@ default_app = firebase_admin.initialize_app(cred_obj, {
 ref = db.reference("/tasks")
 
 # user adds single task onto Firebase
-
-
 def pushTask(taskObj):
     ref.push().set(taskObj)
 
 # retrieves all tasks that falls within the user's selected range
-
-
 def retrieveTasks(userPostal, selectedRange):
     filteredTasks = []
     allTasks = ref.get()
@@ -32,10 +28,10 @@ x = {
     "Category": "Gardening",
     "Desc": "Suck my balls"
 }
-y = json.dumps(x)
+
 
 
 # xdpd = ref.get()
 # for task in xdpd.values():
 #     print(task['Postal'])
-print(distance.calculate_distance(738081, 821308))
+# print(distance.calculate_distance(738081, 821308))
