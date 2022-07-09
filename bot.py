a0273ae9b5bd9382cba580f89b9f23a0f0254107
Jason Baek <bkjwjason@gmail.com>
2022-07-09 21:34:48 +0800

    merging with bot

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
+from cgitb import text
 from faulthandler import cancel_dump_traceback_later
 from setuptools import Command
 import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext, CallbackQueryHandler
 import logging
 import methods 
 
@@ -21,12 +22,20 @@ def main() -> None:
     start_conv_handler = ConversationHandler(
                             entry_points=[CommandHandler('start', methods.start)], 
 
-                            states = {methods.OPTION: [MessageHandler(Filters.text, methods.display_or_add)]},
+                            states = {
+                                methods.OPTION: [MessageHandler(Filters.text, methods.display_or_add)],
+                                methods.PATH: [MessageHandler(Filters.text, methods.path)],
+                                methods.STATE: [MessageHandler(Filters.text, methods.state)],
+                                methods.UPDATE_NAME: [MessageHandler(Filters.text, methods.update_name)],
+                                methods.UPDATE_NUMBER: [MessageHandler(Filters.text, methods.update_number)],
+                                methods.UPDATE_DESC: [MessageHandler(Filters.text, methods.update_desc)],
+                                methods.KEYBOARD : [MessageHandler(Filters.text, methods.keyboard)],
+                                methods.SHOW_DATA: [MessageHandler(Filters.text, methods.show_data)]
+                            },
 
                             fallbacks=[CommandHandler('cancel', methods.cancel)])
 
     dispatcher.add_handler(start_conv_handler)
-
     dispatcher.add_handler(CommandHandler('cancel', methods.cancel))
 
     updater.start_polling()
--- methods.py
+++ methods.py
@@ -1,25 +1,106 @@
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, ChatAction, User
+from email import message
+import telegram
+from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, ChatAction, User
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
+from database import pushJob
 import logging
 
 logger = logging.getLogger(__name__)
-OPTION = range(1)
-information = []
+OPTION, PATH, STATE, VIEW, NAME, UPDATE_NAME, UPDATE_NUMBER, UPDATE_DESC, KEYBOARD, SHOW_DATA, DONE  = range(11)
+information = {"Name": None, "Postal":None, "Desc": None, "Category":None}
+
 
 def start(update: Update, _:CallbackContext):
     #Request for postal code
     update.message.reply_text("Please enter your postal code: ", reply_markup=ReplyKeyboardRemove())
     return OPTION
 
-
 def display_or_add(update: Update, _:CallbackContext):
     #logger displays previous information entered, in this case Postal Code. Adds to an array
     user = update.message.from_user
-    information.append(update.message.text)
+    information["Postal"] = update.message.text
     logger.info("Address of %s: %s", user.first_name, update.message.text)
     reply_keyboard = [['Submit a request', 'View volunteering opportunities']]
-    update.message.reply_text("What would you like to do today?", reply_markup= ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-    return ConversationHandler.END
+    update.message.reply_text("What would you like to do today?", reply_markup= ReplyKeyboardMarkup(reply_keyboard))
+    return PATH
+
+def path(update: Update, _:CallbackContext):
+    if update.message.text == "Submit a request":
+        msg= ("You may choose to add your name, phone number, category of work or the description of work. "
+        "To quit, please type /cancel")
+        reply_keyboard = [["Name", "Category"], ["Description", "Phone Number"], ["Done"]]
+        update.message.reply_text(msg, reply_markup=reply_keyboard)
+        return STATE
+
+def keyboard(update: Update, _:CallbackContext):
+    msg= ("You may choose to add your name, phone number, category of work or the description of work. "
+    "To quit, please type /cancel")
+    reply_keyboard = [["Name", "Category"], ["Description", "Phone Number"], ["Done"]]
+    update.message.reply_text(msg, reply_markup=reply_keyboard)
+    return STATE
+
+def state(update: Update, _:CallbackContext):
+    if update.message.text == "Name":
+        update.message.reply_text("Please enter your name", reply_markup = ReplyKeyboardRemove)
+        return UPDATE_NAME
+    elif update.message.text == "Category":
+        pass
+    elif update.message.text == "Description":
+        update.message.reply_text("Please enter a short description of the request",reply_markup = ReplyKeyboardRemove)
+        return UPDATE_DESC
+    elif update.message.text == "Phone Number":
+        update.message.reply_text("Please enter your phone number")
+        return UPDATE_NUMBER
+    elif update.message.text == "Done":
+        return SHOW_DATA
+
+def update_name(update: Update, _:CallbackContext):
+    information["Name"] = update.message.text
+    return KEYBOARD
+
+def update_number(update: Update, _:CallbackContext):
+    information["Number"] = update.message.text
+    return KEYBOARD
+
+def update_desc(update: Update, _:CallbackContext):
+    information["Desc"] = update.message.text
+    return KEYBOARD
+
+def update_categories(update: Update, _:CallbackContext):
+    information["Category"] = update.message.text
+    return KEYBOARD
+
+def show_data(update: Update, _:CallbackContext):
+    data = ""
+    for key, value in information.items():
+        data = data + key + ": " + value
+    update.message.reply_text("Please check that the information is correct:" + "\n" + data)
+    reply_keyboard = [["Yes", "No"]]
+    update.message.reply_text("Would you like to submit?", reply_markup=reply_keyboard)
+    return DONE
+def categories(update: Update, _: CallbackContext):
+    query = update.callback_query
+    query.answer()
+    category_keys = [
+        [
+        InlineKeyboardButton(text = "Renovation", callback_data="Renovation"),
+        InlineKeyboardButton(text = "Gardening", callback_data='Gardening')
+        ],
+        [   
+        InlineKeyboardButton(text = "Manual Labour", callback_data='Manual Labour'),
+        InlineKeyboardButton(text = "Necessities", callback_data= "Necessities")
+        ],
+    ]
+    keyboard = InlineKeyboardMarkup(category_keys)
+    query.edit_message_text("What kind of job is it?")
+    query.edit_message_reply_markup(keyboard)
+
+def done(update: Update, _: CallbackContext):
+    if update.message.text == "Yes":
+        pushJob(information)
+        update.message.reply_text("Your request has been submitted. Have a great day!")
+    else:
+        return KEYBOARD
 
 def cancel(update: Update, _: CallbackContext):
     user = update.message.from_user
@@ -28,3 +109,6 @@ def cancel(update: Update, _: CallbackContext):
         'You have quit the conversation. Have a great day!'
     )
     return ConversationHandler.END
+
+
+bot = Bot("5553340518:AAHPPct5kqRZ62BXWRCj8_2u9EZnQzKA2tE")
