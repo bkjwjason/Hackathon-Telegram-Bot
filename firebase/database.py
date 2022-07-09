6749bcfacd859b4b754b3a75de5ad11315cc2b66
Rayner Toh <raynertoh@r-171-104-25-172.comp.nus.edu.sg>
2022-07-09 19:23:21 +0800

    Finalised database logic and renaming of file

---
+++ firebase/database.py
@@ -0,0 +1,29 @@
+import firebase_admin
+import json
+from firebase_admin import db
+from distance import *
+
+cred_obj = firebase_admin.credentials.Certificate(
+    "help-a-neighbour-out-firebase-adminsdk-3k1ih-a01592b112.json")
+default_app = firebase_admin.initialize_app(cred_obj, {
+    'databaseURL': 'https://help-a-neighbour-out-default-rtdb.asia-southeast1.firebasedatabase.app/'
+})
+
+jobsRef = db.reference("/jobs")
+
+# pushes single job onto Firebase
+def pushJob(jobObj):
+    jobsRef.push().set(jobObj)
+
+# retrieves all jobs that falls within the user's selected range
+def getJobs(userPostal, selectedRange, selectedCategory):
+    filteredJobs = []
+    allJobs = jobsRef.get()
+    for jobObj in allJobs.values():
+        dist = calculate_distance(userPostal, jobObj['Postal'])
+        if dist <= selectedRange:
+            filteredJobs.append(jobObj)
+    return filteredJobs
+
+        
+
