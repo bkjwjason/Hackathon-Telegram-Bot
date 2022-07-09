03433a8f9b81938d6b93d8b54d20f64c4dc4eaa5
Jason Baek <bkjwjason@gmail.com>
2022-07-10 00:25:15 +0800

    To Heroku

--- bot.py
+++ bot.py
@@ -1,19 +1,22 @@
 from telegram.ext import Updater, CommandHandler
 import logging
 import methods 
-
+import os
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
 def main() -> None:
     # Start bot
-    TOKEN = "TOKEN"
+    TOKEN = "5553340518:AAHPPct5kqRZ62BXWRCj8_2u9EZnQzKA2tE"
     updater = Updater(TOKEN)
+    PORT = int(os.environ.get('PORT', '8443'))
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
     dispatcher.add_handler(methods.start_conv_handler)
     dispatcher.add_handler(CommandHandler('cancel', methods.cancel))
-    updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=TOKEN,webhook_url= 'https://hackathon-tele-bot.herokuapp.com/' + TOKEN)
 
 if __name__ == '__main__':
     main()
